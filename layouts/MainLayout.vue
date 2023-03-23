69f9b86f032c05dc302f4b27131aa39e46c73888
Tapiwa Motsi <tmotsi10@gmail.com>
2023-03-23 19:26:30 +0200

    side bar

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,4 +1,16 @@
 <template>
     <TopNav/>
-    <div class=""></div>
+        <div 
+        :class="route.fullPath === '/' ? 'max-w-[1140px]' : ''" 
+        class="flex justify-between mx-auto w-full lg:px-2.5 px-0"
+    >
+        <div>
+            <SideNavMain />
+        </div>
+        <slot />
+    </div>
 </template>
+
+<script setup>
+const route = useRoute()
+</script>
