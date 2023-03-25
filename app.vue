c92893c2ff91e51a4ed49e35332c11ce8f14f2fd
Tapiwa Motsi <tmotsi10@gmail.com>
2023-03-25 21:06:11 +0200

    state added

--- app.vue
+++ app.vue
@@ -1,4 +1,10 @@
 <template>
-  <NuxtPage/>
+  <NuxtPage v-if="isLoginOpen"/>
   <AuthOverlay/>
 </template>
+<script setup>
+
+import { storeToRefs } from 'pinia';
+const { $userStore, $generalStore } = useNuxtApp()
+const { isLoginOpen, isEditProfileOpen } = storeToRefs($generalStore)
+</script>
